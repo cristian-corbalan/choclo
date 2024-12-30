61bf9e62296724eaff4d86c0a20b24e334a9191e
cristian-corbalan <ccorbalandw@gmail.com>
2024-12-30 15:51:03 -0300

    Create user factory when run seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        User::factory()->make()->save();
+
         $this->call([
             CategorySeeder::class,
             ProductSeeder::class,
