517c12cf0406a96d22bcca8b00e8c9fd6576f3cf
cristian-corbalan <ccorbalandw@gmail.com>
2025-01-08 17:32:17 -0300

    Add colors, sizes and category properties to products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
     //
+    public function colors (): BelongsToMany
+    {
+        return $this->belongsToMany(Color::class, 'product_variants');
+    }
+
+    public function sizes (): BelongsToMany
+    {
+        return $this->belongsToMany(Size::class, 'product_variants');
+    }
+
+    public function category (): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
